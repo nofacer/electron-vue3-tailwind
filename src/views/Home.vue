743a52e6507f5c7479d951e585c893f3565c01ac
nofacer <dustin.ni@outlook.com>
2023-05-29 15:03:06 +0800

    replace vuex with pinia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
   home
+  Counter: {{ store.count }}
+  <div class="bg-sky-300" @click="store.increment()">click</div>
 </template>
 
 <script setup lang="ts">
+import {useCounterStore} from '@/stores/counter'
+
+const store = useCounterStore()
+
 </script>
 
 <style scoped>
